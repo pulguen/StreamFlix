500b004aa1df9323403ef2d632da8c13d9f48169
cortezdavid <david.cortez020@gmail.com>
2023-11-30 16:12:42 -0300

    favoritos ruta privada

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext, useState } from "react"
 import { FavoritesContext } from "../../context/FavoritesContext";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 const Favorites = () => {
   const { getFavorites, deleteFavorite } = useContext(FavoritesContext);
@@ -32,21 +33,23 @@ const Favorites = () => {
   }
 
   return (
-    <div>
-      {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        <div>
-          {favorites.map(favorite => (
-            <div key={favorite.id}>
-              <p>{favorite.name}</p>
-              <img src={favorite.img} alt="" />
-              <button onClick={() => { deleteF(favorite.id) }}>Borrar</button>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <PrivateRoute>
+      <div>
+        {loading ? (
+          <p>Cargando...</p>
+        ) : (
+          <div>
+            {favorites.map(favorite => (
+              <div key={favorite.id}>
+                <p>{favorite.name}</p>
+                <img src={favorite.img} alt="" />
+                <button onClick={() => { deleteF(favorite.id) }}>Borrar</button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </PrivateRoute>
   );
 }
 
