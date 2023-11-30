08af5fa879da2684b6350b807bbd17bd8ddee2bb
pulguen <ricardorojo.dg@gmail.com>
2023-11-30 20:04:11 -0300

    Estilos con bootstrap a login y a registro

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,66 @@
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/AuthContext'
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
+  const { loginEmail, userLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const { loginEmail, userLogin } = useContext(AuthContext)
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
 
-    const navigate = useNavigate()
+  const loguearse = async (e) => {
+    e.preventDefault();
+    await loginEmail(email, pass);
+    navigate('/');
+  };
 
-    const [email, setEmail] = useState("")
-    const [pass, setPass] = useState("")
+  return userLogin ? (
+    <h1>Ya estoy logueado</h1>
+  ) : (
+    <div className="container mt-5 mb-5">
+      <h2 className="text-center">Iniciar sesión</h2>
+      <form className="login-form mx-auto mb-3" style={{ maxWidth: '400px' }} onSubmit={loguearse}>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Email</label>
+          <input
+            type="email"
+            className="form-control text-muted fw-light"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Ingresa tu email"
+          />
+        </div>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Contraseña</label>
+          <input
+            type="password"
+            className="form-control text-muted fw-light"
+            onChange={(e) => setPass(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+          />
+        </div>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-primary w-100">
+            Iniciar sesión
+          </button>
+        </div>
+      </form>
+      <div className="register-link text-center">
+        <p className="d-inline">¿No tienes cuenta?</p>
+        <Link to="/register" className="d-inline">
+          Regístrate
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
 
 
-    const loguearse = async (e) => {
-        e.preventDefault()
-        await loginEmail(email, pass)
-        navigate("/")
-    }
 
-    return (
-        userLogin ? (
-            <h1>ya estoy logueado</h1>
-        ) : (
-            <div>
-                <h2>iniciar sesion</h2>
-                <form style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "20px" }}>
-                    <div>
-                        <label>email</label>
-                        <input onChange={(e) => setEmail(e.target.value)} type="email" />
-                    </div>
-                    <div>
-                        <label>contraseña</label>
-                        <input onChange={(e) => setPass(e.target.value)} type="password" />
-                    </div>
-                    <div>
-                        <button onClick={(e) => loguearse(e)}>iniciar sesion</button>
-                    </div>
-                </form>
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <p>no tenes cuenta?</p>
-                    <Link to={'/register'}>Registrate</Link>
-                </div>
-            </div>)
-    )
-}
 
-export default Login
+
+
+
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,48 +1,62 @@
-import { useContext, useState } from "react"
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from "../../context/AuthContext"
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Register = () => {
+  const { registerEmail, userLogin } = useContext(AuthContext);
+  const [nameRegister, setNameRegister] = useState('');
+  const [emailRegister, setEmailRegister] = useState('');
+  const [passRegister, setPassRegister] = useState('');
+  const navigate = useNavigate();
 
-  const { registerEmail, userLogin } = useContext(AuthContext)
-
-  const [nameRegister, setNameRegister] = useState("")
-  const [emailRegister, setEmailRegister] = useState("")
-  const [passRegister, setPassRegister] = useState("")
-  
-  const navigate = useNavigate()
-  
   const registrar = async (e) => {
-    e.preventDefault()
-    await registerEmail(nameRegister, emailRegister, passRegister)
-    navigate("/")
-  }
+    e.preventDefault();
+    await registerEmail(nameRegister, emailRegister, passRegister);
+    navigate('/');
+  };
 
-  return (
-    userLogin ? (
-      <h1>ya estoy registrado</h1 >
-    ) : (
-      <div>
-        <h2>nuevo usuario</h2>
-        <form style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "20px" }}>
-          <div>
-            <label>nombre</label>
-            <input onChange={(e) => setNameRegister(e.target.value)} type="text" />
-          </div>
-          <div>
-            <label>email</label>
-            <input onChange={(e) => setEmailRegister(e.target.value)} type="email" />
-          </div>
-          <div>
-            <label>contraseña</label>
-            <input onChange={(e) => setPassRegister(e.target.value)} type="password" />
-          </div>
-          <div>
-            <button onClick={(e) => registrar(e)}>confirmar</button>
-          </div>
-        </form>
-      </div>)
-  )
-}
+  return userLogin ? (
+    <h1>Ya estoy registrado</h1>
+  ) : (
+    <div className="container mt-5 mb-5">
+      <h2 className="text-center">Nuevo usuario</h2>
+      <form className="register-form mx-auto" style={{ maxWidth: '400px' }} onSubmit={registrar}>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Nombre</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setNameRegister(e.target.value)}
+            placeholder="Ingresa tu nombre"
+          />
+        </div>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            onChange={(e) => setEmailRegister(e.target.value)}
+            placeholder="Ingresa tu email"
+          />
+        </div>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => setPassRegister(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+          />
+        </div>
+        <div className="mb-5 mt-4">
+          <button type="submit" className="btn btn-primary w-100">
+            Confirmar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
