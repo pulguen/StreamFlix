0c212ab72c01f974c6cb7d67e56a90b06046f886
pulguen <ricardorojo.dg@gmail.com>
2023-12-01 10:47:49 -0300

    no aparece el navbar si no estas logueado, se modifico index.js y app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
+import React, { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import { FavoritesProvider } from './context/FavoritesContext';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Favorites from './components/Favorites/Favorites';
-import MovieDetails from './components/MovieDetails/MovieDetails'
+import MovieDetails from './components/MovieDetails/MovieDetails';
 import SearchResults from './components/SearchResults/SearchResults';
 import Footer from './components/Footer/Footer';
 import 'swiper/css';
@@ -14,11 +16,18 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 function App() {
+  const { userLogin, authChecked } = useContext(AuthContext);
+
+  if (!authChecked) {
+    // Aquí puedes agregar un indicador de carga si es necesario
+    return <div>Cargando...</div>;
+  }
+
   return (
     <AuthProvider>
       <FavoritesProvider>
         <BrowserRouter>
-          <Navbar />
+          {userLogin && <Navbar />}
           <Routes>
             <Route exact path='/' element={<Home />} />
             <Route exact path='/*' element={<Home />} />
@@ -36,3 +45,4 @@ function App() {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+
