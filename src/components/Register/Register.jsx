1c6ee846ef87d76df9cfe0f0d0fcc819309b06eb
pulguen <ricardorojo.dg@gmail.com>
2023-12-11 16:05:08 -0300

    register con sweetalert2

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
+import Swal from 'sweetalert2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Register = () => {
@@ -9,12 +10,44 @@ const Register = () => {
   const [nameRegister, setNameRegister] = useState('');
   const [emailRegister, setEmailRegister] = useState('');
   const [passRegister, setPassRegister] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
   const navigate = useNavigate();
 
   const registrar = async (e) => {
     e.preventDefault();
-    await registerEmail(nameRegister, emailRegister, passRegister);
-    navigate('/');
+
+    // Verifica si las contraseñas coinciden
+    if (passRegister !== confirmPass) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Contraseñas no coinciden',
+        text: 'Las contraseñas ingresadas no coinciden. Por favor, inténtalo de nuevo.',
+      });
+      return;
+    }
+
+    try {
+      await registerEmail(nameRegister, emailRegister, passRegister);
+      navigate('/');
+
+      // Muestra SweetAlert2 después de un registro exitoso
+      Swal.fire({
+        icon: 'success',
+        title: 'Registro exitoso',
+        text: '¡Te has registrado exitosamente!',
+        timer: 1500,
+        showConfirmButton: false,
+      });
+    } catch (error) {
+      console.error(error);
+
+      // Muestra SweetAlert2 en caso de error en el registro
+      Swal.fire({
+        icon: 'error',
+        title: 'Error en el registro',
+        text: 'Hubo un problema al intentar registrarte. Por favor, inténtalo de nuevo.',
+      });
+    }
   };
 
   return userLogin ? (
@@ -50,13 +83,22 @@ const Register = () => {
             placeholder="Ingresa tu contraseña"
           />
         </div>
+        <div className="mb-3 d-flex flex-column align-items-start">
+          <label className="form-label fw-bold">Confirmar Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => setConfirmPass(e.target.value)}
+            placeholder="Confirma tu contraseña"
+          />
+        </div>
         <div className="mb-5 mt-4">
           <button type="submit" className="btn btn-primary w-100">Confirmar</button>
+        </div>
         <div>
           <Link to="/login">
             <button className="btn btn-primary w-100 mt-2">Volver</button>
           </Link>
-          </div>
         </div>
       </form>
     </div>
